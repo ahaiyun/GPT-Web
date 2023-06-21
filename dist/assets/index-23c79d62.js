71610b4a8118d7d0b9d02ae81e8ef23a75356660
“ahaiyun” <hlzhao1017@163.com>
2023-06-21 20:47:49 +0800

    First commit

---
+++ dist/assets/index-23c79d62.js
@@ -0,0 +1,1 @@
+import{a6 as a,j as s,ap as i,B as e}from"./index-69a0c86a.js";function n(){const t=a();return s.jsx(i,{status:"success",title:"支付成功!",subTitle:"",extra:[s.jsx(e,{onClick:()=>{t("/")},children:"返回首页"},"home"),s.jsx(e,{type:"primary",onClick:()=>{t("/shop")},children:"查看记录"},"shop")]})}export{n as default};
